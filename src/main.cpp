59e75a1d2b21b25072d7b56f25fe9deb7d594e97
prog-yammer <367671@niuitmo.ru>
2025-04-13 13:13:42 +0300

    fix

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,9 @@
 #include "web/server.h"
-#include "web/common/tools.h"
 
 int main()
 {
-    std::string qew = "-1 0 qoidwj";
-    auto m = getMessage(qew);
-    std::cout << "";
-//    Server server(std::thread::hardware_concurrency(), 8080);
-//    server.start();
+    Server server(std::thread::hardware_concurrency(), 8080);
+    server.start();
 
     return 0;
 }
